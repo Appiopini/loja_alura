3d103ee2f50cc5fe7b217d699b4e1f136f2dbe0b
Appiopini <appiopini@gmail.com>
2022-10-06 17:55:58 -0300

    Form to create new product

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-
-  # get "produtos", to: "produtos#index" begginig
+  
+  post "produtos", to: "produtos#create"
+  get "produtos/new", to: "produtos#new"
   root to: "produtos#index" 
 
 end
